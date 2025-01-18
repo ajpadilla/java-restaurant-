9da48b411ef5f5ace91c8e89f5e270e8be55dd89
Alvaro Padilla <ajpadilla88@gmail.com>
2025-01-18 12:01:24 -0500

    Agregando request para ingredientes

--- store/src/main/java/restaurant/store/order/infrastructure/request/IngredientRequest.java
+++ store/src/main/java/restaurant/store/order/infrastructure/request/IngredientRequest.java
@@ -1,4 +1,27 @@
 package restaurant.store.order.infrastructure.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class IngredientRequest {
+
+    private String id;
+    private String name;
+    private int quantity; // Usa int o BigDecimal según el caso
+
+    @Override
+    public String toString() {
+        return "IngredientRequest{" +
+                "ingredientId='" + id + '\'' +
+                ", ingredientName='" + name + '\'' +
+                ", quantity=" + quantity +
+                '}';
+    }
+
 }
