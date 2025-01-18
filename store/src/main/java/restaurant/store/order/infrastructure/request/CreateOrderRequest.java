3720b4e39a176989398bc6b9ad3635fec629d57e
Alvaro Padilla <ajpadilla88@gmail.com>
2025-01-18 12:03:14 -0500

    Agregando request para ordenes

--- store/src/main/java/restaurant/store/order/infrastructure/request/CreateOrderRequest.java
+++ store/src/main/java/restaurant/store/order/infrastructure/request/CreateOrderRequest.java
@@ -1,4 +1,27 @@
 package restaurant.store.order.infrastructure.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateOrderRequest {
+
+    private String orderId;
+    private List<PlateRequest> plates;
+
+    @Override
+    public String toString() {
+        return "CreateOrderRequest{" +
+                "orderId='" + orderId + '\'' +
+                ", plates=" + plates +
+                '}';
+    }
+
 }
