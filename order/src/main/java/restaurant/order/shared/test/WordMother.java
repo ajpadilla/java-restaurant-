2a9545ca4ab5269c66d9e21d705ca8d5c8fa0ab8
Alvaro Padilla <ajpadilla88@gmail.com>
2025-01-18 15:40:48 -0500

    Agregando object mother base

--- order/src/main/java/restaurant/order/shared/test/WordMother.java
+++ order/src/main/java/restaurant/order/shared/test/WordMother.java
@@ -1,7 +1,19 @@
 package restaurant.order.shared.test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public final class WordMother {
+    private static final List<String> INGREDIENTS = Arrays.asList(
+            "Tomato", "Lemon", "Potato", "Rice", "Ketchup",
+            "Lettuce", "Onion", "Cheese", "Meat", "Chicken"
+    );
+
+    private static final Random RANDOM = new Random();
+
     public static String random() {
-        return MotherCreator.random().lorem().word();
+        // Selecciona un ingrediente aleatorio de la lista
+        return INGREDIENTS.get(RANDOM.nextInt(INGREDIENTS.size()));
     }
 }
