bbf86e9b9b93762d97fd789946a644cef8b9a4aa
Alvaro Padilla <ajpadilla88@gmail.com>
2025-01-18 15:41:12 -0500

    Agregando configuraciones de paypal

--- order/src/main/java/restaurant/order/config/PayPalConfig.java
+++ order/src/main/java/restaurant/order/config/PayPalConfig.java
@@ -1,4 +1,25 @@
 package restaurant.order.config;
 
+import com.paypal.base.rest.APIContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+
+@Configuration
 public class PayPalConfig {
+
+    @Value("${paypal.client-id}")
+    private String clientId;
+
+    @Value("${paypal.client-secret}")
+    private String clientSecret;
+
+    @Value("${paypal.mode}")
+    private String mode;
+
+    @Bean
+    public APIContext apiContext() {
+        return new APIContext(clientId, clientSecret, mode);
+    }
+
 }
