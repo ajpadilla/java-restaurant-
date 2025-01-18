95da808eb293fabd7ed6c23c3a807840cfb3159e
Alvaro Padilla <ajpadilla88@gmail.com>
2025-01-18 11:56:41 -0500

    Modificando entidad

--- store/src/main/java/restaurant/store/purchases/infrastructure/entity/PurchaseEntity.java
+++ store/src/main/java/restaurant/store/purchases/infrastructure/entity/PurchaseEntity.java
@@ -2,8 +2,6 @@ package restaurant.store.purchases.infrastructure.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import restaurant.store.ingredients.domain.Ingredient;
-import restaurant.store.ingredients.infrastructure.entity.IngredientEntity;
 import restaurant.store.purchases.domain.Purchase;
 import restaurant.store.purchases.domain.PurchaseDescription;
 import restaurant.store.purchases.domain.PurchaseId;
@@ -28,18 +26,12 @@ public class PurchaseEntity {
     @Column(nullable = false)
     private Integer quantity;
 
-    // Relación con Ingredient
-    @ManyToOne
-    @JoinColumn(name = "ingredient_id", nullable = false)
-    private IngredientEntity ingredientEntity;
-
     public static PurchaseEntity fromDomain(Purchase purchase) {
         return PurchaseEntity
                 .builder()
                 .id(purchase.getId().getValue())
                 .description(purchase.getDescription().getValue())
                 .quantity(purchase.getQuantity().getValue())
-                .ingredientEntity(IngredientEntity.fromDomain(purchase.getIngredient()))
                 .build();
     }
 
@@ -47,8 +39,7 @@ public class PurchaseEntity {
         return new Purchase(
                 new PurchaseId(this.id),
                 new PurchaseDescription(this.description),
-                new PurchaseQuantity(this.quantity),
-                this.ingredientEntity.toDomain()
+                new PurchaseQuantity(this.quantity)
         );
     }
 
