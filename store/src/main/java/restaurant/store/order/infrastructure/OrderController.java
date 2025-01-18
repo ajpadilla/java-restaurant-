a1f4ca16777187ca4ca05648ca623a9b774c213e
Alvaro Padilla <ajpadilla88@gmail.com>
2025-01-18 12:03:51 -0500

    Agregando Controlador para ordenes

--- store/src/main/java/restaurant/store/order/infrastructure/OrderController.java
+++ store/src/main/java/restaurant/store/order/infrastructure/OrderController.java
@@ -1,4 +1,37 @@
 package restaurant.store.order.infrastructure;
 
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import restaurant.store.ingredients.application.AvailabilityIngredientService;
+import restaurant.store.order.infrastructure.request.CreateOrderRequest;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
+    @Autowired
+    private final AvailabilityIngredientService ingredientService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<HashMap<String, Serializable>> save(@RequestBody CreateOrderRequest request) {
+        logger.info("Received CreateOrderRequest: {}", request);
+        HashMap<String, Serializable> orderMap = this.ingredientService.processOrder(request);
+        return ResponseEntity.ok(orderMap);
+    }
+
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Health check passed - valid role!");
+    }
 }
