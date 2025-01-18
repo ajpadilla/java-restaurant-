e81ebc064b54381136f88494760c4fc9658f2678
Alvaro Padilla <ajpadilla88@gmail.com>
2025-01-18 15:43:18 -0500

    Agregando integer mother

--- order/src/main/java/restaurant/order/shared/test/IntegerMother.java
+++ order/src/main/java/restaurant/order/shared/test/IntegerMother.java
@@ -3,7 +3,7 @@ package restaurant.order.shared.test;
 public final class IntegerMother {
 
     public static Integer random() {
-        return MotherCreator.random().number().numberBetween(1, 100);
+        return MotherCreator.random().number().numberBetween(1, 5);
     }
 
 
