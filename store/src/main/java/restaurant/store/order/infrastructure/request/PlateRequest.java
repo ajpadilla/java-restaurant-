c5b0ca754404691af06a0d0b6f3354498d3a4a08
Alvaro Padilla <ajpadilla88@gmail.com>
2025-01-18 12:00:34 -0500

    Definiendo entidad resquest para platos

--- store/src/main/java/restaurant/store/order/infrastructure/request/PlateRequest.java
+++ store/src/main/java/restaurant/store/order/infrastructure/request/PlateRequest.java
@@ -1,4 +1,27 @@
 package restaurant.store.order.infrastructure.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class PlateRequest {
+    private String plateId;
+    private String plateName;
+    private List<IngredientRequest> ingredients;
+
+    @Override
+    public String toString() {
+        return "PlateRequest{" +
+                "plateId='" + plateId + '\'' +
+                ", plateName='" + plateName + '\'' +
+                ", ingredients=" + ingredients +
+                '}';
+    }
 }
