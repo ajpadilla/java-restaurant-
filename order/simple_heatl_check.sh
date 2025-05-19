eddef977e6692f31e16924da592caf6fe883c9bc
Alvaro Padilla <ajpadilla88@gmail.com>
2025-05-19 08:48:39 -0500

    Updting scrip

--- order/simple_heatl_check.sh
+++ order/simple_heatl_check.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+source ./network_utils.sh
+
 URL="http://localhost:8081/api/v1/orders/health"
+INTERFACE=$(get_active_interface)
+
 STATUS=$(curl -s -o /dev/null -w "%{http_code}" $URL)
+RECEIVED_PKTS=$(cat /sys/class/net/$INTERFACE/statistics/rx_packets)
+DROPPED_PKTS=$(cat /sys/class/net/$INTERFACE/statistics/rx_dropped)
 
 if [ "$STATUS" -ne 200 ]; then
   echo "NGINX is DOWN! Status: $STATUS"
 else
   echo "NGINX is running fine. Status: $STATUS"
 fi
+
+echo "Interface:" $INTERFACE
+echo "Received Packets:" $RECEIVED_PKTS
+echo "Dropped Packets:" $DROPPED_PKTS
