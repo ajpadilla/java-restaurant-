df103ad9f64c5f76cc2f7b8ae692e66ef2335da2
Alvaro Padilla <ajpadilla88@gmail.com>
2025-05-20 15:26:49 -0500

    Updating script

--- order/create_virtual_network.sh
+++ order/create_virtual_network.sh
@@ -4,52 +4,36 @@ set -e
 set -x
 
 BRIDGE_NAME=br0
+VETH_APP=veth1
+VETH_BRIDGE=veth1-br
 BRIDGE_IP=192.168.100.1/24
-
-# APP namespace setup
-APP_NS=appns
-VETH_APP=veth-app
-VETH_APP_BR=veth-app-br
 APP_IP=192.168.100.101/24
 
-# CLIENT namespace setup
-CLIENT_NS=clientns
-VETH_CLIENT=veth-client
-VETH_CLIENT_BR=veth-client-br
-CLIENT_IP=192.168.100.102/24
+# Create bridge if it doesn't exist
+if ! ip link show "$BRIDGE_NAME" &>/dev/null; then
+    ip link add "$BRIDGE_NAME" type bridge
+    ip addr add "$BRIDGE_IP" dev "$BRIDGE_NAME"
+    ip link set "$BRIDGE_NAME" up
+fi
 
-echo "[*] Cleaning up..."
-ip netns del $APP_NS 2>/dev/null || true
-ip netns del $CLIENT_NS 2>/dev/null || true
-ip link del $VETH_APP_BR 2>/dev/null || true
-ip link del $VETH_CLIENT_BR 2>/dev/null || true
-ip link del $BRIDGE_NAME 2>/dev/null || true
+# Delete old veth pair if exists
+ip link del "$VETH_APP" &>/dev/null || true
 
-echo "[+] Creating bridge..."
-ip link add $BRIDGE_NAME type bridge
-ip addr add $BRIDGE_IP dev $BRIDGE_NAME
-ip link set $BRIDGE_NAME up
+# Create veth pair
+ip link add "$VETH_APP" type veth peer name "$VETH_BRIDGE"
 
-# ---- APP Side ----
-echo "[+] Setting up app namespace..."
-ip netns add $APP_NS
-ip link add $VETH_APP type veth peer name $VETH_APP_BR
-ip link set $VETH_APP netns $APP_NS
-ip link set $VETH_APP_BR master $BRIDGE_NAME
-ip link set $VETH_APP_BR up
-ip netns exec $APP_NS ip addr add $APP_IP dev $VETH_APP
-ip netns exec $APP_NS ip link set $VETH_APP up
-ip netns exec $APP_NS ip link set lo up
+# Attach one end to the bridge
+ip link set "$VETH_BRIDGE" master "$BRIDGE_NAME"
+ip link set "$VETH_BRIDGE" up
 
-# ---- CLIENT Side ----
-echo "[+] Setting up client namespace..."
-ip netns add $CLIENT_NS
-ip link add $VETH_CLIENT type veth peer name $VETH_CLIENT_BR
-ip link set $VETH_CLIENT netns $CLIENT_NS
-ip link set $VETH_CLIENT_BR master $BRIDGE_NAME
-ip link set $VETH_CLIENT_BR up
-ip netns exec $CLIENT_NS ip addr add $CLIENT_IP dev $VETH_CLIENT
-ip netns exec $CLIENT_NS ip link set $VETH_CLIENT up
-ip netns exec $CLIENT_NS ip link set lo up
+# Bring up the app side
+ip link set "$VETH_APP" up
+ip addr add "$APP_IP" dev "$VETH_APP"
 
-echo "[✔] Network is ready!"
+# Enable IP forwarding so packets can flow through VM if needed
+sysctl -w net.ipv4.ip_forward=1
+
+# Optional: add NAT rules if you want to route traffic between VM and host
+
+echo "[*] Setup complete. Bridge $BRIDGE_NAME with $APP_IP on $VETH_APP created."
+
