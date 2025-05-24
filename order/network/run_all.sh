ad7577b3429d8bfdb912cf0afdd76209d4513506
Alvaro Padilla <ajpadilla88@gmail.com>
2025-05-24 10:19:44 -0500

    Change path

--- order/network/run_all.sh
+++ order/network/run_all.sh
@@ -6,6 +6,6 @@ bash monitor/monitor_flow.sh &
 sleep 2  # Let tcpdump start first
 
 echo "[*] Sending traffic with wrk..."
-python3 client/send_wrk.py
+python3 client/python/send_wrk.py
 
 echo "[✓] All tasks completed. Check logs/ folder."
