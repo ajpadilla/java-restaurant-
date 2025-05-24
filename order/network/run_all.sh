9017ebf975667887c5efa72d5759382d13fe1c09
Alvaro Padilla <ajpadilla88@gmail.com>
2025-05-24 10:35:32 -0500

    Updating paths

--- order/network/run_all.sh
+++ order/network/run_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "[*] Running monitoring script..."
-bash monitor/monitor_flow.sh &
+bash /home/orders/java-restaurant/order/network/monitor/monitor_flow.sh &
 
 sleep 2  # Let tcpdump start first
 
 echo "[*] Sending traffic with wrk..."
-python3 client/python/send_wrk.py
+python3 python3 /home/orders/java-restaurant/order/network/client/python/send_wrk.py
 
 echo "[✓] All tasks completed. Check logs/ folder."
