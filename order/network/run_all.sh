894a0ce2b825c94fbf24429313e401d80a6bfac6
Alvaro Padilla <ajpadilla88@gmail.com>
2025-07-17 15:37:51 -0500

    Updating

--- order/network/run_all.sh
+++ order/network/run_all.sh
@@ -6,6 +6,6 @@ bash /home/orders/java-restaurant/order/network/monitor/monitor_flow.sh &
 sleep 2  # Let tcpdump start first
 
 echo "[*] Sending traffic with wrk..."
-python3 python3 /home/orders/java-restaurant/order/network/client/python/send_wrk.py
+python3  /home/orders/java-restaurant/order/network/client/python/send_wrk.py
 
 echo "[✓] All tasks completed. Check logs/ folder."
