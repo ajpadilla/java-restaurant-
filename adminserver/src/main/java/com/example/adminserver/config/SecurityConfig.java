e9e3ba5d5951f96d620d7e76f102154851cea55e
Alvaro Padilla <ajpadilla88@gmail.com>
2025-09-03 19:47:57 -0500

    Adding Admin panel security config

--- adminserver/src/main/java/com/example/adminserver/config/SecurityConfig.java
+++ adminserver/src/main/java/com/example/adminserver/config/SecurityConfig.java
@@ -1,4 +1,24 @@
 package com.example.adminserver.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**", "/instances/**", "/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()) // basic auth
+                .csrf(AbstractHttpConfigurer::disable);       // disable CSRF for Admin Client
+        return http.build();
+    }
 }
