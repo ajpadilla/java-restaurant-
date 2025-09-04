9db8266427364debbc66a228558518bdb8f19f00
Alvaro Padilla <ajpadilla88@gmail.com>
2025-09-03 20:17:38 -0500

    Updating script to run the app

--- order/start_service.sh
+++ order/start_service.sh
@@ -6,8 +6,10 @@ mkdir -p logs
 # Clear or create orders.log
 > logs/orders.log
 
-# Start Order service
-echo "Starting Order service..."
-nohup java -jar target/order-0.0.1-SNAPSHOT.jar > logs/orders.log 2>&1 &
-
-
+# Start Order service with perf-map-agent and diagnostic options
+echo "Starting Order service with perf-map-agent..."
+nohup java \
+  -agentpath:/home/alvaro/perf-map-agent/perf-map-agent/out/libperfmap.so \
+  -XX:+UnlockDiagnosticVMOptions \
+  -XX:+DebugNonSafepoints \
+  -jar target/order-0.0.1-SNAPSHOT.jar > logs/orders.log 2>&1 &
