7c28855146426ef872a567f00ae711175dbfda2b
Alvaro Padilla <ajpadilla88@gmail.com>
2025-09-03 20:18:03 -0500

    Updating test

--- order/src/test/java/restaurant/order/plate/application/CreatePlateCommandHandlerShould.java
+++ order/src/test/java/restaurant/order/plate/application/CreatePlateCommandHandlerShould.java
@@ -90,7 +90,7 @@ public class CreatePlateCommandHandlerShould {
             System.out.println("Occurred On: " + event[4]);
         }*/
 
-        this.consumer.consume();
+        //this.consumer.consume();
         //this.consumer.stop();
 
     }
