ca3d6a0074ca233195dd12b19589c8348c83c4c7
Alvaro Padilla <ajpadilla88@gmail.com>
2025-09-03 20:25:45 -0500

    Updating bus

--- order/src/main/java/restaurant/order/shared/domain/bus/event/EventBus.java
+++ order/src/main/java/restaurant/order/shared/domain/bus/event/EventBus.java
@@ -1,7 +1,10 @@
 package restaurant.order.shared.domain.bus.event;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 public interface EventBus {
+    @Transactional
     void publish(final List<DomainEvent> events);
 }
