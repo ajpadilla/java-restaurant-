be0f3c12e2c4857bc46374ea3eb84826727155b5
Alvaro Padilla <ajpadilla88@gmail.com>
2025-09-09 07:51:44 -0500

    Adding script to create a plate

--- order/create_plate.sh
+++ order/create_plate.sh
@@ -2,24 +2,33 @@
 
 API_URL="http://192.168.100.101:8081/api/v1/plates/create"
 
-# Preloaded ingredient IDs in your in-memory DB
-INGREDIENTS=("11111111-1111-1111-1111-111111111111" "22222222-2222-2222-2222-222222222222")
+# Preloaded ingredients with full info
+# Format: "id|name|requiredQuantity"
+INGREDIENTS=(
+  "11111111-1111-1111-1111-111111111111|Tomato|2"
+  "22222222-2222-2222-2222-222222222222|Cheese|1"
+)
 
 for i in {1..5}; do
   plate_id=$(uuidgen)
   plate_name="plate_$i"
 
-  # Build JSON array for ingredients
-  ingredients_json=$(printf '"%s",' "${INGREDIENTS[@]}")
-  ingredients_json="[${ingredients_json%,}]"
+  # Build JSON array of objects
+  ingredients_json="["
+  for ingredient in "${INGREDIENTS[@]}"; do
+    IFS="|" read -r id name qty <<< "$ingredient"
+    ingredients_json+="{\"ingredientId\":\"$id\",\"ingredientName\":\"$name\",\"requiredQuantity\":$qty},"
+  done
+  # Remove trailing comma and close array
+  ingredients_json="${ingredients_json%,}]"
 
   sudo ip netns exec clientns curl -s -X POST "$API_URL" \
     -H "Content-Type: application/json" \
     -d "{
       \"id\": \"$plate_id\",
       \"name\": \"$plate_name\",
-      \"ingredientsIds\": $ingredients_json
+      \"ingredients\": $ingredients_json
     }"
 
-  echo "Created plate $plate_name with ingredients ${INGREDIENTS[*]}"
+  echo "Created plate $plate_name with ingredients: $ingredients_json"
 done
