0d425f7550304424a1b09ba1ab20b847c5adb290
Alvaro Padilla <ajpadilla88@gmail.com>
2025-09-09 07:52:42 -0500

    Adding service to ensure plates values

---
+++ order/src/main/java/restaurant/order/menu/domain/PlateService.java
@@ -0,0 +1,26 @@
+package restaurant.order.menu.domain;
+
+import org.springframework.stereotype.Service;
+import restaurant.order.shared.domain.InvalidUUIDException;
+import restaurant.order.shared.domain.Utils;
+
+@Service
+public class PlateService {
+
+    private final PlateRepository repository;
+
+
+    public PlateService(PlateRepository repository) {
+        this.repository = repository;
+    }
+
+    public Plate findAnExistingPlate(PlateId id) {
+
+        if (!Utils.isUUID(id.getValue())) {
+            throw new InvalidUUIDException("Invalid UUID string: " + id.getValue());
+        }
+
+        return this.repository.search(id).orElseThrow(()-> new PlateNotFoundException("Ingredient not found: " + id.getValue()));
+    }
+
+}
