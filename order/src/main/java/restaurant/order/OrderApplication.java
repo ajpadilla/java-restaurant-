5cbe238717bd83a1e8fce0f8ef523d918efa66fc
Alvaro Padilla <ajpadilla88@gmail.com>
2025-09-09 08:10:01 -0500

    Updating application init

--- order/src/main/java/restaurant/order/OrderApplication.java
+++ order/src/main/java/restaurant/order/OrderApplication.java
@@ -1,9 +1,16 @@
 package restaurant.order;
 
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import restaurant.order.menu.domain.Ingredient;
+import restaurant.order.menu.domain.IngredientId;
+import restaurant.order.menu.domain.IngredientName;
+import restaurant.order.menu.domain.IngredientQuantity;
+import restaurant.order.menu.infrastructure.api.InMemoryInventoryClientService;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,5 +19,22 @@ public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner loadIngredients(InMemoryInventoryClientService ingredientService) {
+        return args -> {
+            ingredientService.saveIngredient(
+                    new Ingredient(new IngredientId("11111111-1111-1111-1111-111111111111"),
+                            new IngredientName("Tomato"),
+                            new IngredientQuantity(5))
+            );
+            ingredientService.saveIngredient(
+                    new Ingredient(new IngredientId("22222222-2222-2222-2222-222222222222"),
+                            new IngredientName("Cheese"),
+                            new IngredientQuantity(3))
+            );
+            System.out.println("Preloaded ingredients: " + ingredientService.getAllIngredients());
+        };
+    }
 }
+
+
