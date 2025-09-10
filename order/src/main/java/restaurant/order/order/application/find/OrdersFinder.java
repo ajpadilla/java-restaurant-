5e850f773928cb3e7f2f4a85ce5f5a4000d039bb
Alvaro Padilla <ajpadilla88@gmail.com>
2025-09-10 08:25:33 -0500

    Adding cache process

--- order/src/main/java/restaurant/order/order/application/find/OrdersFinder.java
+++ order/src/main/java/restaurant/order/order/application/find/OrdersFinder.java
@@ -1,18 +1,36 @@
 package restaurant.order.order.application.find;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 import restaurant.order.order.application.find.dto.OrderResponse;
-import restaurant.order.order.domain.Order;
 import restaurant.order.order.domain.OrderRepository;
+import restaurant.order.shared.cache.Cache;
 
 @Service
 public class OrdersFinder {
     private final OrderRepository repository;
-    public OrdersFinder(OrderRepository repository) {
+
+    private final Cache cache;
+
+    public OrdersFinder(OrderRepository repository, Cache cache) {
         this.repository = repository;
+        this.cache = cache;
     }
     public Page<OrderResponse> find(int page, int size) {
-        return this.repository.searchAll(page, size);
+        String key = "orders:page=" + page + ":size=" + size;
+
+        Page<OrderResponse> cached = cache.get(
+                key,
+                new TypeReference<PageImpl<OrderResponse>>() {}
+        );
+
+        if (cached != null) return cached;
+
+        Page<OrderResponse> ordersPage = repository.searchAll(page, size);
+        cache.put(key, ordersPage, 300);
+
+        return ordersPage;
     }
 }
--- order/src/main/java/restaurant/order/shared/cache/Cache.java
+++ order/src/main/java/restaurant/order/shared/cache/Cache.java
@@ -1,4 +1,11 @@
 package restaurant.order.shared.cache;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+
 public interface Cache {
+    <T> T get(String key, Class<T> type);
+    <T> T get(String key, TypeReference<T> typeRef);
+    void put(String key, Object value, long ttlSeconds);
+    void evict(String key);
+    void evictByPrefix(String prefix);
 }
