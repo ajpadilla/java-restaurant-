c55e77eaaa52c87fcf2385c6a62c479b2cc6fcfc
Alvaro Padilla <ajpadilla88@gmail.com>
2025-09-11 08:55:37 -0500

    Defining async bus implementation

--- order/src/main/java/restaurant/order/shared/Infrastructure/bus/command/AsyncCommandBus.java
+++ order/src/main/java/restaurant/order/shared/Infrastructure/bus/command/AsyncCommandBus.java
@@ -1,4 +1,35 @@
 package restaurant.order.shared.Infrastructure.bus.command;
 
-public class AsyncCommandBus {
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+import restaurant.order.shared.domain.bus.command.Command;
+import restaurant.order.shared.domain.bus.command.CommandBus;
+import restaurant.order.shared.domain.bus.command.CommandHandler;
+import restaurant.order.shared.domain.bus.command.CommandNotRegisteredError;
+
+import java.util.concurrent.ExecutorService;
+
+@Service
+public class AsyncCommandBus implements CommandBus {
+
+    private final CommandHandlersInformation information;
+    private final ApplicationContext context;
+    private final ExecutorService commandExecutor;
+
+    public AsyncCommandBus(CommandHandlersInformation information,
+                           ApplicationContext context,
+                           @Qualifier("commandExecutor") ExecutorService commandExecutor) {
+        this.information = information;
+        this.context = context;
+        this.commandExecutor = commandExecutor;
+    }
+
+    @Override
+    public void dispatch(Command command) throws CommandNotRegisteredError {
+            Class<? extends CommandHandler> handlerClass = information.search(command.getClass());
+            CommandHandler handler = context.getBean(handlerClass);
+
+            commandExecutor.submit(() -> handler.handle(command));
+    }
 }
