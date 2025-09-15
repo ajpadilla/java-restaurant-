b5f11dbb3595c54d4a0c23bf421a0ca035e7cb36
Alvaro Padilla <ajpadilla88@gmail.com>
2025-09-15 13:44:33 -0500

    Updating code

--- order/src/main/java/restaurant/order/shared/Infrastructure/bus/command/CommandHandlersInformation.java
+++ order/src/main/java/restaurant/order/shared/Infrastructure/bus/command/CommandHandlersInformation.java
@@ -8,43 +8,31 @@ import restaurant.order.shared.domain.bus.command.CommandNotRegisteredError;
 
 import java.lang.reflect.ParameterizedType;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
 
 @Service
 public final class CommandHandlersInformation {
-    HashMap<Class<? extends Command>, Class<? extends CommandHandler>> indexedCommandHandlers;
 
-    public CommandHandlersInformation() {
-        Reflections                          reflections = new Reflections("restaurant.order");
-        Set<Class<? extends CommandHandler>> classes     = reflections.getSubTypesOf(CommandHandler.class);
+    private final HashMap<Class<? extends Command>, CommandHandler<?>> indexedCommandHandlers = new HashMap<>();
 
-        this.indexedCommandHandlers = formatHandlers(classes);
-    }
-
-    private HashMap<Class<? extends Command>, Class<? extends CommandHandler>> formatHandlers(
-            Set<Class<? extends CommandHandler>> commandHandlers
-    ) {
-        HashMap<Class<? extends Command>, Class<? extends CommandHandler>> handlers = new HashMap<>();
-
-        for (Class<? extends CommandHandler> handler : commandHandlers) {
-            ParameterizedType        paramType    = (ParameterizedType) handler.getGenericInterfaces()[0];
-            Class<? extends Command> commandClass = (Class<? extends Command>) paramType.getActualTypeArguments()[0];
-
-            System.out.println(handler + " " + paramType + " " + commandClass);
-
-            handlers.put(commandClass, handler);
+    public CommandHandlersInformation(List<CommandHandler<?>> handlers) {
+        for (CommandHandler<?> handler : handlers) {
+            Class<? extends Command> commandClass = extractCommandClass(handler);
+            indexedCommandHandlers.put(commandClass, handler);
         }
-
-        return handlers;
     }
 
-    public Class<? extends CommandHandler> search(Class<? extends Command> commandClass) throws CommandNotRegisteredError {
-        Class<? extends CommandHandler> commandHandlerClass = this.indexedCommandHandlers.get(commandClass);
+    private Class<? extends Command> extractCommandClass(CommandHandler<?> handler) {
+        ParameterizedType paramType = (ParameterizedType) handler.getClass().getGenericInterfaces()[0];
+        return (Class<? extends Command>) paramType.getActualTypeArguments()[0];
+    }
 
-        if (null == commandHandlerClass) {
+    public CommandHandler<?> search(Class<? extends Command> commandClass) throws CommandNotRegisteredError {
+        CommandHandler<?> handler = indexedCommandHandlers.get(commandClass);
+        if (handler == null) {
             throw new CommandNotRegisteredError(commandClass);
         }
-
-        return commandHandlerClass;
+        return handler;
     }
 }
