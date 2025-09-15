d108cf12087ff94d600a5ee453e85bf7c8fce9f2
Alvaro Padilla <ajpadilla88@gmail.com>
2025-09-15 13:42:10 -0500

    Updating code to get beans

--- order/src/main/java/restaurant/order/shared/Infrastructure/bus/query/QueryHandlersInformation.java
+++ order/src/main/java/restaurant/order/shared/Infrastructure/bus/query/QueryHandlersInformation.java
@@ -3,60 +3,50 @@ package restaurant.order.shared.Infrastructure.bus.query;
 
 import org.reflections.Reflections;
 import org.springframework.stereotype.Service;
+import restaurant.order.menu.application.find.*;
+import restaurant.order.order.application.find.FindOrdersQueryHandler;
+import restaurant.order.shared.domain.bus.command.CommandHandler;
 import restaurant.order.shared.domain.bus.query.Query;
 import restaurant.order.shared.domain.bus.query.QueryHandler;
 import restaurant.order.shared.domain.bus.query.QueryNotRegisteredError;
 
 import java.lang.reflect.ParameterizedType;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 @Service
 public final class QueryHandlersInformation {
 
-    private final HashMap<Class<? extends Query>, Class<? extends QueryHandler<?, ?>>> indexedQueryHandlers;
+    private final HashMap<Class<? extends Query>, QueryHandler<?, ?>> indexedQueryHandlers = new HashMap<>();
 
-    public QueryHandlersInformation() {
-        Reflections reflections = new Reflections("restaurant.order");
-        Set<Class<? extends QueryHandler>> classes = reflections.getSubTypesOf(QueryHandler.class);
+    public QueryHandlersInformation(List<QueryHandler<?, ?>> handlers) {
+        for (QueryHandler<?, ?> handler : handlers) {
+            Class<? extends Query> queryClass = extractQueryClass(handler);
+            indexedQueryHandlers.put(queryClass, handler);
+        }
 
-        this.indexedQueryHandlers = formatHandlers(classes);
+        // Debug log
+        indexedQueryHandlers.forEach((query, handler) ->
+                System.out.println("Registered query: " + query.getSimpleName() + " -> " + handler.getClass().getSimpleName()));
     }
 
-    /**
-     * Search for the QueryHandler class associated with a given Query class.
-     */
-    public Class<? extends QueryHandler<?, ?>> search(Class<? extends Query> queryClass) throws QueryNotRegisteredError {
-        Class<? extends QueryHandler<?, ?>> queryHandlerClass = indexedQueryHandlers.get(queryClass);
-
-        if (queryHandlerClass == null) {
+    @SuppressWarnings("unchecked")
+    public <Q extends Query, R> QueryHandler<Q, R> search(Class<Q> queryClass) throws QueryNotRegisteredError {
+        QueryHandler<?, ?> handler = indexedQueryHandlers.get(queryClass);
+        if (handler == null) {
             throw new QueryNotRegisteredError(queryClass);
         }
-
-        return queryHandlerClass;
+        return (QueryHandler<Q, R>) handler;
     }
 
-    /**
-     * Builds a map of Query -> QueryHandler class.
-     */
-    @SuppressWarnings("unchecked")
-    private HashMap<Class<? extends Query>, Class<? extends QueryHandler<?, ?>>> formatHandlers(
-            Set<Class<? extends QueryHandler>> queryHandlers
-    ) {
-        HashMap<Class<? extends Query>, Class<? extends QueryHandler<?, ?>>> handlers = new HashMap<>();
-
-        for (Class<? extends QueryHandler> handler : queryHandlers) {
-            // Extract the first generic interface implemented by the handler
-            ParameterizedType paramType = (ParameterizedType) handler.getGenericInterfaces()[0];
-
-            // The first type argument is the Query class
-            Class<? extends Query> queryClass = (Class<? extends Query>) paramType.getActualTypeArguments()[0];
-
-            // Store with proper generic parameterization
-            handlers.put(queryClass, (Class<? extends QueryHandler<?, ?>>) handler);
-        }
-
-        return handlers;
+    private Class<? extends Query> extractQueryClass(QueryHandler<?, ?> handler) {
+        // Use reflection to get generic type
+        return (Class<? extends Query>) ((ParameterizedType) handler.getClass()
+                .getGenericInterfaces()[0])
+                .getActualTypeArguments()[0];
     }
 }
