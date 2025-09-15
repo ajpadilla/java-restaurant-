91ef0da927cc357a6c589696b80cf93bad9ebc35
Alvaro Padilla <ajpadilla88@gmail.com>
2025-09-15 13:42:48 -0500

    Refactoring code to get query bus

--- order/src/main/java/restaurant/order/shared/Infrastructure/bus/query/InMemoryQueryBus.java
+++ order/src/main/java/restaurant/order/shared/Infrastructure/bus/query/InMemoryQueryBus.java
@@ -4,24 +4,21 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 import restaurant.order.shared.domain.bus.query.*;
 
+import java.util.List;
+
 @Service
 public final class InMemoryQueryBus implements QueryBus {
     private final QueryHandlersInformation information;
-    private final ApplicationContext context;
 
-    public InMemoryQueryBus(QueryHandlersInformation information, ApplicationContext context) {
-        this.information = information;
-        this.context     = context;
+    public InMemoryQueryBus(List<QueryHandler<?, ?>> handlers) {
+        this.information = new QueryHandlersInformation(handlers);
     }
 
-    @Override
     @SuppressWarnings("unchecked")
+    @Override
     public <R> R ask(Query query) throws QueryHandlerExecutionError {
         try {
-            Class<? extends QueryHandler<?, ?>> queryHandlerClass = information.search(query.getClass());
-
-            QueryHandler<Query, R> handler = (QueryHandler<Query, R>) context.getBean(queryHandlerClass);
-
+            QueryHandler<Query, R> handler = (QueryHandler<Query, R>) information.search(query.getClass());
             return handler.handle(query);
         } catch (Throwable error) {
             throw new QueryHandlerExecutionError(error);
